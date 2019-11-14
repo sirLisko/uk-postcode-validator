8808ecf2e84ca63c90c06d30b0556906784437b1
Luca Lischetti <luca@sirlisko.com>
2019-11-14 13:54:36 +0100

    added typescript types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 const POSTCODE_VALIDATION_REGEXT = /^([Gg][Ii][Rr]0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$/;
 
-module.exports = postcode =>
+/**
+ * `uk-postcode-validator` validate UK postcodes.
+ * @param {string} postcode The postcode that needs to be validated
+ */
+const validator = (postcode: string): boolean =>
   Boolean(postcode.replace(" ", "").match(POSTCODE_VALIDATION_REGEXT));
+
+export default validator;
